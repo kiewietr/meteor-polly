65e0ac742f1e82ccd4d2a16891b94ed129fbce4e
kiewietr <kiewietr@gmail.com>
2016-02-25 09:42:39 +0000

    added version constraints && fixed typo in documentation

--- package.js
+++ package.js
@@ -3,10 +3,12 @@ Package.describe({
   summary: "Package to add Polly Help Center to your meteor app. Add simple link with {{> pollyHelpLink}}",
   version: "1.0.0",
   git: "https://github.com/kiewietr/meteor-polly",
-  documentation: "README.md"
+  documentation: "readme.md"
 });
 
 Package.on_use(function(api){
+    api.versionsFrom( '1.2.0.2' );
+    
     api.use(["templating", "jquery", "less"], "client");
 
     api.add_files([
